520a6e56c8527ca8acb141e0cc3d3c9902e3bd90
Swaraj Ashok Langhe <langheswaraj@gmail.com>
2026-06-22 20:52:48 +0530

    Add files via upload

---
+++ C_Programming/Practice/program0019.c
@@ -0,0 +1,44 @@
+// Program to calculate Ticket price by age group 
+
+#include <stdio.h>
+
+int CalculateTicketPrice(int iAge)
+{
+    // Input Updator
+    if(iAge < 0)
+    {
+        iAge = - iAge;
+    }
+
+    if(iAge >= 0 && iAge <= 5)
+    {
+        return 0;
+    }
+    else if(iAge >= 6 && iAge <= 18)
+    {
+        return 500;
+    }
+    else if (iAge >= 19 && iAge <= 50)
+    {
+        return 900;
+    }
+    else 
+    {
+        return 400;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Please Enter your age to calculate ticket prices : ");
+    scanf("%d",&iValue);
+
+    iRet = CalculateTicketPrice(iValue);
+
+    printf("Your Ticket orice will be %d rs\n", iRet);
+
+    return 0;
+}   
