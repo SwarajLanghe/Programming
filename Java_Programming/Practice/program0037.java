207988d7331fa25eae13f85166a6cf61cdd6b600
Swaraj Ashok Langhe <langheswaraj@gmail.com>
2026-06-24 23:55:19 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program0037.java
@@ -0,0 +1,14 @@
+// print 1 to 10 numbers in ascending order
+
+class program0037
+{
+    public static void main(String A[])
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= 10; iCnt++)
+        {
+            System.out.println(iCnt);
+        }
+    }
+}
