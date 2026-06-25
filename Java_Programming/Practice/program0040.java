6845e956382e013e7fcdf2c0e64f6a3218b59cce
Swaraj Ashok Langhe <langheswaraj@gmail.com>
2026-06-25 21:35:22 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program0040.java
@@ -0,0 +1,31 @@
+// Input Number is divisible by 3 and 5 or not check
+
+// Type 2
+
+import java.util.Scanner;
+
+class program0040 
+{
+    void CheckDivisible(int iNo)
+    {
+        if((iNo % 3 == 0) && (iNo % 5 == 0))
+            {
+                System.out.println("Numer is divisible by 3 and 5");
+            }
+            else
+            {
+                System.out.println("Numer is not divisible by 3 and 5");
+            }
+    }
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt();
+
+        CheckDivisible(iValue);     // Error 
+    }
+}
