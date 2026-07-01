abfcc5bbe44377752607b766f195abedef8f3897
Swaraj Ashok Langhe <langheswaraj@gmail.com>
2026-07-01 20:12:38 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program0065.java
@@ -0,0 +1,55 @@
+// Program to check that the input number is prime number or not 
+
+import java.util.Scanner;
+
+class NumberX
+{
+    public boolean CheckPrime(int iNo)
+    {
+        int iCnt = 0;
+  
+        for(iCnt = 2; iCnt <= (iNo / 2) ; iCnt++)
+        {
+            if((iNo % iCnt) == 0)
+            {
+                break;
+            } 
+        }
+
+        if(iCnt >= (iNo/2)+1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class program0065
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+        Boolean bRet = false;
+
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt();
+
+        NumberX nobj = new NumberX();
+
+        bRet = nobj.CheckPrime(iValue);
+
+            if(bRet == true)
+            {
+                System.out.println("It is Prime");
+            }
+            else
+            {
+                System.out.println("It is not Prime");
+            }        
+    }
+}
