29af3bc833b1903ac98c21b6f41440f69612436a
Swaraj Langhe <langheswaraj@gmail.com>
2026-07-16 21:55:19 +0530

    Add files via upload

---
+++ C_Programming/Competitive/Programs/A4_e.c
@@ -0,0 +1,41 @@
+//write a program which accept number from user and difference between summation of all its factors and non-factor
+//12
+//-34 
+
+#include<stdio.h>
+
+int FactDiff(int iNo)
+{
+    int iCnt = 0;
+    int iFactSum = 0;
+    int iNonFactSum = 0;
+
+    for(iCnt = 1; iCnt < iNo; iCnt++)
+    {
+        if((iNo % iCnt) == 0)
+        {
+            iFactSum = iFactSum + iCnt;
+        }
+        else
+        {
+            iNonFactSum = iNonFactSum + iCnt;
+        }
+    }
+
+    return (iFactSum - iNonFactSum);
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = FactDiff(iValue);
+
+    printf("%d",iRet);
+
+    return 0;
+}
