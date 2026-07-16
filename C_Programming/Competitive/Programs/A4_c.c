712eae1cfb8ddec46fbb79f245492b20aa4f2b35
Swaraj Langhe <langheswaraj@gmail.com>
2026-07-16 21:53:34 +0530

    Add files via upload

---
+++ C_Programming/Competitive/Programs/A4_c.c
@@ -0,0 +1,29 @@
+//write a program which accept number from user and display all its non-factors
+//12
+//5 7 8 9 10 11
+#include<stdio.h>
+
+void NonFact(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt < iNo; iCnt++)
+    {
+        if((iNo % iCnt) != 0)
+        {
+            printf("%d\t",iCnt);
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    NonFact(iValue);
+
+    return 0;
+}
